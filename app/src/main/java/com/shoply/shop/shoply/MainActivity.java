296b4dc9569b8442ee4fc5c958ac82bbc4216267
daniellag <daniellag@google.com>
2015-04-07 21:24:03 +0300

    added the search box

--- app/src/main/java/com/shoply/shop/shoply/MainActivity.java
+++ app/src/main/java/com/shoply/shop/shoply/MainActivity.java
@@ -1,16 +1,11 @@
 package com.shoply.shop.shoply;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceView;
 import android.view.SurfaceHolder;
-import android.view.MenuInflater;
-import android.widget.SearchView;
+import android.view.SurfaceView;
 
 
 public class MainActivity extends ActionBarActivity {
